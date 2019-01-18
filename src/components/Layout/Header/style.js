8cb24ae308366280840077a6867000a1c57ab37d
aikai18561710520 <546622461@qq.com>
2019-01-18 09:41:36 +0800

    修改错误样式

--- src/components/Layout/Header/style.js
+++ src/components/Layout/Header/style.js
@@ -1,29 +1,34 @@
-import styled from 'styled-components'
-import { Icon } from 'antd'
-export const HeaderBox = styled.div `
-    width:100%;
-    color:#fff;
-    font-size:20px;
-    font-weight:bold;
+import styled from "styled-components"
+import { Icon } from "antd"
+export const HeaderBox = styled.div`
+    width: 100%;
+    color: #fff;
+    font-size: 20px;
+    font-weight: bold;
 `
 
-export const UserName = styled.div `
-    cursor:point;
-    padding-right:24px;
+export const UserName = styled.div`
+    cursor: pointer;
+    padding-right: 24px;
+    &:hover {
+        span.user_name {
+            color: #ccc;
+        }
+    }
     span.user_name {
         font-size: #eee;
         font-size: 16px;
         margin-left: 4px;
+        transition: all 0.2s;
     }
 `
 
-export const TriggerIcon = styled(Icon)
-`
+export const TriggerIcon = styled(Icon)`
     font-size: 18px;
     line-height: 64px;
     padding: 0 24px;
     cursor: pointer;
-    transition: color .3s;
+    transition: color 0.3s;
     &:hover {
         color: #1890ff;
     }
